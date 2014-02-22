f12b7cec70008ef90b755e0acef1bcec15dcf7a3
Ankur <ankurmit2006@gmail.com>
2014-02-22 17:24:50 -0500

    Cimmiting for backup.

--- hw1/sys_xconcat.c
+++ hw1/sys_xconcat.c
@@ -3,6 +3,7 @@
 #include <linux/moduleloader.h>
 #include <asm/segment.h>
 #include <asm/uaccess.h>
+#define BUFFER_SIZE BUFFER_SIZE
 struct syscall_params {
         const char *outfile; // name of output file
         const char **infiles; // array with names of input files
@@ -15,22 +16,55 @@ struct syscall_params {
 asmlinkage extern long (*sysptr)(void *arg);
 
 /*
- * Function Declarations.
+  *Function Declarations.
  */
-struct file* file_open(const char* path, int flags, int rights);
-int file_write(struct file* file, unsigned long long offset,
-		unsigned char* data, unsigned int size);
-int file_read(struct file* file, unsigned long long offset,
-		unsigned char* data, unsigned int size);
-void file_close(struct file* file);
+struct file *file_open(const char *path, int flags, int rights);
+int file_write(struct file *file, unsigned long long offset,
+		unsigned char *data, unsigned int size);
+int file_read(struct file *file, unsigned long long offset,
+		unsigned char *data, unsigned int size);
+void file_close(struct file *file);
+int read_write(struct syscall_params *params)
+{
+	int i,ret,count,bytes_written=0;
+	struct file *outfile = file_open(params->outfile,
+			params->oflags,mode);
+	struct file *infile;
+	//TODO:Check for outputfile error
+	for(i=0;i<params->infile_count;i++){
+		ret = 0;
+		count = 0;
+		//TODO:need to check read access
+		infile = file_open(params->infiles[i],O_RDONLY,0);
+		//TODO: Check error Condition
+		do{
+			ret = file_read(infile,count*BUFFER_SIZE,
+				data,BUFFER_SIZE);
+			if(ret > BUFFER_SIZE)
+				data[ret] = 0;
+			count++;
+			file_write(outfile,bytes_written,
+				data, BUFFER_SIZE);
+			bytes_written += ret;
+		}while(ret != BUFFER_SIZE);
+	}
 
+}
 asmlinkage long xconcat(void *arg)
 {
+	unsigned char data[BUFFER_SIZE];
+	struct file *infile;
+	int ret;
 	struct syscall_params *p;
 	if (arg == NULL)
 		return -EINVAL;
 	else {
-		p = (struct syscall_params*)arg;
+		p = arg;
+		infile = file_open("test.txt",O_RDONLY,0);
+		//TODO: Check error Condition
+		ret = file_read(infile,0,data,BUFFER_SIZE);
+		data[ret] = 0;
+		printk("%d, %s", ret, data);
 		printk("%d",p->outfile[1]);
 		return 0;
 	}
@@ -53,8 +87,8 @@ module_init(init_sys_xconcat);
 module_exit(exit_sys_xconcat);
 MODULE_LICENSE("GPL");
 
-struct file* file_open(const char* path, int flags, int rights) {
-	struct file* filp = NULL;
+struct file *file_open(const char *path, int flags, int rights) {
+	struct file *filp = NULL;
 	mm_segment_t oldfs;
 	int err = 0;
 
@@ -69,12 +103,12 @@ struct file* file_open(const char* path, int flags, int rights) {
 	return filp;
 }
 
-void file_close(struct file* file) {
+void file_close(struct file *file) {
     filp_close(file, NULL);
 }
 
-int file_read(struct file* file, unsigned long long offset,
-		unsigned char* data, unsigned int size) {
+int file_read(struct file *file, unsigned long long offset,
+		unsigned char *data, unsigned int size) {
     mm_segment_t oldfs;
     int ret;
 
@@ -87,8 +121,8 @@ int file_read(struct file* file, unsigned long long offset,
     return ret;
 }
 
-int file_write(struct file* file, unsigned long long offset,
-		unsigned char* data, unsigned int size) {
+int file_write(struct file *file, unsigned long long offset,
+		unsigned char *data, unsigned int size) {
     mm_segment_t oldfs;
     int ret;
 
